915a004cdcaf745d60163efe4e994ebc7787a757
Mani Kant Kumar <manikantraj060@gmail.com>
2026-01-22 17:00:28 +0530

    update

---
+++ Butterfly_P.java
@@ -0,0 +1,35 @@
+public class Butterfly_P {
+    public static void main(String[] args) {
+        int n = 5;
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// output:-
+
+
